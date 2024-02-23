aa237993d683ee3e49c3338090484180aaea45d5
Pouya Gharibpour <p.gharibpour@gmail.com>
2024-02-23 09:25:28 +0330

    added game create view test

--- posts/tests/test_views.py
+++ posts/tests/test_views.py
@@ -4,12 +4,19 @@ from posts.models import Genre, Game, Rating, GameDevRole
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.contrib.auth.models import User
 from games import settings
+from ..forms import GameForm
 
 import shutil
 
 class TestPostsGamesViews(TestCase):
     @classmethod
     def setUpTestData(cls) -> None:
+        image_bytes = None
+        file_bytes = None
+        with open("media/1x1.png", "rb") as image:
+            image_bytes = image.read()
+        with open('media/test.zip', 'rb') as file:
+            file_bytes = file.read()
         User.objects.create(**{'username':"user1", 'password':"123456789))"})
         User.objects.create(**{'username':"user2", 'password':"123456789)"})
         Genre.objects.create(name="FPS", slug="fps")
@@ -19,10 +26,10 @@ class TestPostsGamesViews(TestCase):
                             author_id = 1,
                             genre_id = 1,
                             video_url = "someurl.com",
-                            file = SimpleUploadedFile("best_file_eva.txt",
-                                                        b"these are the file contents!"),
-                            image = SimpleUploadedFile("best_fa.txt",
-                                                        b"these are the file contents!"),
+                            file = SimpleUploadedFile("test.zip",
+                                                        file_bytes),
+                            image = SimpleUploadedFile("1x1.png",
+                                                        image_bytes),
                             is_published = True)
         game1.tags.add("tag1")
         
@@ -31,10 +38,10 @@ class TestPostsGamesViews(TestCase):
                             author_id = 1,
                             genre_id = 2,
                             video_url = "someurl.com",
-                            file = SimpleUploadedFile("best_file_eva.txt",
-                                                        b"these are the file contents!"),
-                            image = SimpleUploadedFile("best_fa.txt",
-                                                        b"these are the file contents!"),
+                            file = SimpleUploadedFile("test.zip",
+                                                        file_bytes),
+                            image = SimpleUploadedFile("1x1.png",
+                                                        image_bytes),
                             is_published = True)
         game2.tags.add("tag2")
         
@@ -43,10 +50,10 @@ class TestPostsGamesViews(TestCase):
                             author_id = 1,
                             genre_id = 1,
                             video_url = "someurl.com",
-                            file = SimpleUploadedFile("best_file_eva.txt",
-                                                        b"these are the file contents!"),
-                            image = SimpleUploadedFile("best_fa.txt",
-                                                        b"these are the file contents!"),
+                            file = SimpleUploadedFile("test.zip",
+                                                        file_bytes),
+                            image = SimpleUploadedFile("1x1.png",
+                                                        image_bytes),
                             is_published = False)
         game3.tags.add("tag3")
         
@@ -56,6 +63,8 @@ class TestPostsGamesViews(TestCase):
     @classmethod
     def tearDownClass(cls) -> None:
         shutil.rmtree(settings.test.MEDIA_ROOT, ignore_errors=True)
+        for game in Game.objects.all():
+            game.delete()
         super().tearDownClass()
 
 
@@ -109,3 +118,35 @@ class TestPostsGamesViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIsNotNone(response.context['form'])
         self.assertTemplateUsed(response, "posts/game/form.html")
+
+    def test_games_create_view_post_with_valid_data(self):
+        login = self.client.force_login(User.objects.first())
+        
+        self.assertEqual(Game.published_games.count(), 2)
+
+        #this should work as long as the tests are run from the main folder, where manage.py is.
+        image_bytes = None
+        file_bytes = None
+        with open("media/1x1.png", "rb") as image:
+            image_bytes = image.read()
+        with open('media/test.zip', 'rb') as file:
+            file_bytes = file.read()
+        response = self.client.post(reverse("posts:game_create"), data={
+            "name": 'some game',
+            "slug": 'some-game',
+            "description": 'testsing osijfoei j',
+            "author": 1,
+            "genre": 1,
+            "video_url": "someurl.com",
+            "file": SimpleUploadedFile("test.zip",
+                                        file_bytes),
+            "image": SimpleUploadedFile("best_fa.png",
+                                        image_bytes, content_type='image/png'),
+            "tags": "tag1",
+            "is_published": True,
+        })
+        # print(response)
+        # print(response.content)
+        self.assertEqual(Game.published_games.count(), 3)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(True)
