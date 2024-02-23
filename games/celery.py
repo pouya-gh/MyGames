6778e0ffd5080fdc31847066337edfd0475a657f
Pouya Gharibpour <p.gharibpour@gmail.com>
2024-02-23 14:51:33 +0330

    fix celery default settings module

--- games/celery.py
+++ games/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'games.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'games.settings.local')
 
 app = Celery('games')
 app.config_from_object('django.conf:settings', namespace='CELERY')
