78b4dcdf4f94d12e24fe3a0cc1ba2610bed9ff8c
Pouya Gharibpour <p.gharibpour@gmail.com>
2024-04-09 22:45:50 +0330

    add debug_toolbar

--- games/settings/local.py
+++ games/settings/local.py
@@ -2,9 +2,17 @@ from .base import *
 
 DEBUG = True
 
+INSTALLED_APPS += ["debug_toolbar"]
+
+MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + MIDDLEWARE
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
