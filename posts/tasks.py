805ab8f2ee07850ff9f1a0058bce198f24dd630e
Pouya Gharibpour <p.gharibpour@gmail.com>
2024-09-16 09:45:41 +0330

    remove geocoder because of boycotts

--- posts/tasks.py
+++ posts/tasks.py
@@ -1,7 +1,9 @@
 from django.core.exceptions import ObjectDoesNotExist
 from celery import shared_task
 import zipfile
-import geocoder
+
+import urllib.request
+import json
 
 import posts
 
@@ -18,13 +20,14 @@ def extract_gamefile(new_game_path, new_game_file):
 
 @shared_task
 def find_ip_location(ip):
-    result = geocoder.ip(ip)
-    location = f"{result.country}:{result.city}"
-    print(location)
-    try:
-        record: posts.models.SiteVisitTracker = posts.models.SiteVisitTracker.objects.filter(ip=ip).first()
-        record.location = location
-        record.save()
-    except ObjectDoesNotExist:
-        pass
-    return True
+    with urllib.request.urlopen(f"http://ip-api.com/json/{ip}") as url:
+        data = url.read().decode()
+        j = json.loads(data)
+        location = f"{j['country']}:{j['regionName']}:{j['city']}"
+        try:
+            record: posts.models.SiteVisitTracker = posts.models.SiteVisitTracker.objects.filter(ip=ip).first()
+            record.location = location
+            record.save()
+        except ObjectDoesNotExist:
+            pass
+        return True
